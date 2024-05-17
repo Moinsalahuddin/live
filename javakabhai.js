654b0493124d74da47fb76e181148fd04a8f2bb8
moinuddin <moinsalahuddin334@gmail.com>
2024-05-17 23:55:01 +0500

    first commit

---
+++ javakabhai.js
@@ -0,0 +1,9 @@
+function love_u(){
+    alert("i love u");
+     
+     
+      
+     
+  }
+   
+  
---
+++ script.js
@@ -0,0 +1,15 @@
+var name_of_gali = prompt("enter your");
+
+        if (name_of_gali === "shazaib"  ) {
+            document.write("shzaib is the gando");
+            
+        }
+            else if (name_of_gali ==="moin" ){
+                 document.write("very good gai");
+            }
+            else if (name_of_gali ==="bushara" ){
+                document.write("rundi ki baxhi");
+            }
+         else {
+            document.write("kuch");
+        }
